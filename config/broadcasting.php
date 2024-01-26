e428b70ba53821c2167167ce3d10cc615c8bc361
Steve <steve@newicon.net>
2024-01-26 11:28:01 +0000

    more annoying pusher fixes

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -33,10 +33,10 @@ return [
 		'pusher' => [
 			'driver' => 'pusher',
 			'key' => env('PUSHER_APP_KEY', ''),
-			'secret' => env('PUSHER_APP_SECRET'),
-			'app_id' => env('PUSHER_APP_ID'),
+			'secret' => env('PUSHER_APP_SECRET', ''),
+			'app_id' => env('PUSHER_APP_ID', ''),
 			'options' => [
-				'cluster' => env('PUSHER_APP_CLUSTER'),
+				'cluster' => env('PUSHER_APP_CLUSTER', 'eu'),
 				'encrypted' => true,
 				'useTLS' => true,
 			],
